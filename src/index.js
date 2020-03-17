eebe82f35b2e496929cc390fffd0287b96d8868b
altman44 <48921520+altman44@users.noreply.github.com>
2020-03-17 14:01:18 -0300

    initial commit
    Base del proyecto (node_modules, index.js, routes/index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+//Initializing
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Routes
+app.use(require('./routes'));
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port ', app.get('port'));
+});
