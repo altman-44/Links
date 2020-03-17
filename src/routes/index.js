9154c5e788ba63626e81660d917c8301c56a7871
altman44 <48921520+altman44@users.noreply.github.com>
2020-03-17 15:11:11 -0300

    ruta nueva nada mas
    solo agregue una ruta nueva

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hola');
+    res.send('secondaries/home');
 });
+
+router.post('/login')
 
 module.exports = router;
