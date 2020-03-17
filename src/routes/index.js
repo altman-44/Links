0009022c3753e7652e94292812c06dcf7be429e7
altman44 <48921520+altman44@users.noreply.github.com>
2020-03-17 15:45:41 -0300

    Verificacion de Usuario
    (base de datos trucha.js)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,36 @@
-const { Router } = require('express');
-const router = Router();
+const express = require('express');
+const router = express.Router();
+const session = require('express-session');
+
+session.users = require('./database/trucha');
+
+app.use(session({
+    secret: "123",
+    resave: true,
+    saveUninitialized: true
+}));
 
 router.get('/', (req, res) => {
-    res.send('secondaries/home');
+    res.render('secondaries/login');
 });
 
-router.post('/login')
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    let user_exists = false;
+    let index;
+
+    for(i = 0; i < session.users.length; i++){
+        if(req.body.username == session.users[i].username){
+            session.username = req.body.username;
+            user_exists = true;
+            index = i;
+        }
+    }
+    if (user_exists){
+        res.render('/home', {fullname: session.data[i].fullname});
+    }else{
+        res.render('/login', {userNoExist = true});
+    }
+});
 
 module.exports = router;
