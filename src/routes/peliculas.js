80732c044839df7ea96c8eb7b6324b1d653c8353
altman44 <48921520+altman44@users.noreply.github.com>
2020-03-20 02:13:21 -0300

    Sigo con la carga de las imagenes (appHome.js)
    Estoy tratando de insertar las imágenes según concuerde con el tipo de película (popular, trending...)

--- src/routes/peliculas.js
+++ src/routes/peliculas.js
@@ -4,7 +4,15 @@ const router = express.Router();
 const imagenes = require('../database/imagenes.js');
 
 router.post('/imagenes', (req, res) => {
-    res.send(imagenes);
+    console.log(req.body);
+    let imagenesAEnviar = [];
+    imagenes.forEach((imagen, i) => {
+        if(imagen.genre[i] == req.body.type){
+            console.log(i);
+            imagenesAEnviar[imagenesAEnviar.length] = imagen;
+        }
+    });
+    res.send(imagenesAEnviar);
 });
 
 module.exports = router;
